af87dac1804510280531e6b8b337b6feef46cbeb
whathow <marwa@gmail.com>
2024-03-21 19:48:41 +0200

    fix axios

--- final/src/Utils/AxiosInstance.js
+++ final/src/Utils/AxiosInstance.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-    baseURL: 'http://localhost:5300',
+    baseURL: process.env.REACT_APP_BACKEND_ENDPOINT,
     withCredentials: true
 })
 
